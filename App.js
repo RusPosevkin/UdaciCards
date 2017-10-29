67000fd09010cd52828e364849bcabecc27a30e8
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-10-29 04:35:46 +0300

    feat: add NewDeckView component and TabNavigator

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DeckListView from './components/DeckListView';
+import NewDeckView from './components/NewDeckView';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import configureStore from './configureStore';
+import { TabNavigator } from 'react-navigation';
 
 const store = configureStore();
+
+const Tabs = TabNavigator({
+  DeckListView: {
+    screen: DeckListView,
+    navigationOptions: {
+      tabBarLabel: 'DeckListView'
+    },
+  },
+  NewDeckView: {
+    screen: NewDeckView,
+    navigationOptions: {
+      tabBarLabel: 'Add New Deck'
+    },
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <View style={[styles.debug, { flex: 1 }]}>
-          <DeckListView />
+          <Tabs />
         </View>
       </Provider>
     );
---
+++ components/NewDeckView.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default class DeckListView extends Component {
+  onSubmit() {
+
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>What is the title of your new deck?</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <TouchableOpacity
+          onPress={this.onSubmit}
+        >
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+};
